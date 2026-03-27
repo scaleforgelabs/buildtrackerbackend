b72d81100b57326a50eb886c8174b8a0730a9f7b
Abdullateef2006 <ojugbelelateef2006@gmail.com>
2026-03-27 11:01:44 +0100

    fixed payment issue

--- buildtracker__backend/settings.py
+++ buildtracker__backend/settings.py
@@ -184,11 +184,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -201,6 +196,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+# Fix for missing admin styles on Windows
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 # Media files
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
